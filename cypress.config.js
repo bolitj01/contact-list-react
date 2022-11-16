04a31d68d43de775f2909f7d1e8007b58181331f
Thomas Bolinger <bolitj01@pfw.edu>
2022-11-16 02:37:48 -0500

    Cypress CI Workflow

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3002',
+    baseUrl: 'http://localhost:3000',
     experimentalStudio: true
   }
 })
